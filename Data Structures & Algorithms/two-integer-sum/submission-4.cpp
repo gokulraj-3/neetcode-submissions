f6338b994d4eae6314b21bdd6aefe6888b2910fb
GOKUL RAJ <gokulraj102002@gmail.com>
2026-05-03 18:18:14 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]]  = i + 1;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            int diff = target - nums[i];
+            if(mp[diff] && i != mp[diff] - 1){
+                return {i,mp[diff] - 1};
+            }
+        }
+
+
+    }
+};
