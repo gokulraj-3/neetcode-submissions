2ee990427a3324601d911346c927fdbeec1c86d7
GOKUL RAJ <gokulraj102002@gmail.com>
2026-05-05 01:20:07 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        //store the count in map
+        //key:value = number:count
+        unordered_map<int,int> freq;
+        for(int i = 0; i < nums.size(); i++){
+            freq[nums[i]] += 1;
+        }
+        //create a min_heap
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> heap;
+        for(pair<int,int> p : freq){
+            heap.push({p.second,p.first});
+            if(heap.size() > k){
+                heap.pop();
+            }
+        }
+        vector<int> res;
+        for(int i = 0; i < k; i++){
+            res.push_back(heap.top().second);
+            heap.pop();
+        }
+        return res;
+       
+    }
+};
