90d1f7fc68d6719ac13f60c55290b2a6336dfa1c
GOKUL RAJ <gokulraj102002@gmail.com>
2026-05-10 17:47:41 +0530

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int buy = prices[0];
+        for(int i = 1; i < prices.size(); i++){
+            if(buy > prices[i]){
+                buy = prices[i];
+            }
+            profit = max((prices[i] - buy), profit);
+        }
+        return profit;
+    }
+};
+
+//starting buy_position = price[0]
+//profit = 0 
+// profit = max(buy - current_price/prices[i], profit)
