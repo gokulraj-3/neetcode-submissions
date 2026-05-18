5c85d1d73394e4396a5d31a7a6d8338917901952
GOKUL RAJ <gokulraj102002@gmail.com>
2026-05-19 00:56:35 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    char inverse(char c){
+        if(c == '}') return '{';
+        if(c == ']') return '[';
+        if(c == ')') return '(';
+        return c;
+    }
+
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i< s.size(); i++){
+            if(s[i] == '[' || s[i] == '{' || s[i] == '(' ){
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty() && inverse(s[i])== st.top()){
+                    st.pop();
+                }
+                else{
+                    return false;
+                } 
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
+
+
+
+
