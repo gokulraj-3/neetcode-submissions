b1311b8eb9473daccad52e4f5ad6a6ef4f463c7e
GOKUL RAJ <gokulraj102002@gmail.com>
2026-05-21 23:23:59 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        int n = temperatures.size();
+        vector<int> ans(n,0);
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                int diff = st.top() - i;
+                ans[i] = diff;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
